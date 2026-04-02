7dc21b5a25998c1beab5c92b9837f17112b9ebb8
has-bii <hasbii.rofficial@gmail.com>
2026-04-02 19:39:43 +0700

    feat: add authentication page on settings

---
+++ frontend/src/app/dashboard/settings/authentication/page.tsx
@@ -0,0 +1,11 @@
+import MainPage from "@/components/sidebar/main-page"
+import { SiteHeader } from "@/components/sidebar/site-header"
+
+export default function AuthenticationPage() {
+  return (
+    <>
+      <SiteHeader label="Authentication" />
+      <MainPage></MainPage>
+    </>
+  )
+}
