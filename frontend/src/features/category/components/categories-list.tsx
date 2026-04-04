70884ce20a10bd8e5e7081d175d2bf87113c10ab
has-bii <hasbii.rofficial@gmail.com>
2026-04-04 15:51:23 +0700

    ui: add empty state ui if category is empty

--- frontend/src/features/category/components/categories-list.tsx
+++ frontend/src/features/category/components/categories-list.tsx
@@ -3,7 +3,9 @@
 import { getCategoriesQueryOptions } from "../api/get-categories.query"
 import { useGetCategoryFilter } from "../hooks/use-get-category-filter"
 import { CategoryCard } from "./category-card"
+import EmptyState from "@/components/empty-state"
 import { useSuspenseQuery } from "@tanstack/react-query"
+import { LayersIcon } from "lucide-react"
 
 export default function CategoriesList() {
   const { type } = useGetCategoryFilter()
@@ -12,6 +14,14 @@ export default function CategoriesList() {
 
   return (
     <>
+      {data.length === 0 && (
+        <EmptyState
+          title="Category Empty"
+          description="You don't have any category. Create expense/income categories."
+          icon={LayersIcon}
+          className="border border-dashed"
+        />
+      )}
       {data.map((category) => (
         <CategoryCard key={category.id} {...category} />
       ))}
